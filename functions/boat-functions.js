48554a2fc54fe097a8afaf5f8359437d0f3a7883
Paul Newling - iMac <PaulNewling@Gmail.com>
2021-08-23 16:38:21 -0700

    cleaned up and asyc/await'd boat-functions

--- functions/boat-functions.js
+++ functions/boat-functions.js
@@ -1,8 +1,6 @@
-/* eslint-disable no-restricted-syntax */
 const { BOATS, datastore } = require('./helper-functions/datastore-helpers');
 const { getSpecificCargo } = require('./cargo-functions');
 
-// Sets the boat data of a boat object
 function setBoatData(boatObject) {
   const boatDetails = {
     id: boatObject[datastore.KEY].id,
@@ -15,10 +13,8 @@ function setBoatData(boatObject) {
   return boatDetails;
 }
 
-// Creates a new boat object in the datastore
-function postBoats(name, type, length, owner) {
+async function postBoats(name, type, length, owner) {
   const key = datastore.key(BOATS);
-
   const newBoat = {
     name,
     type,
@@ -26,109 +22,67 @@ function postBoats(name, type, length, owner) {
     owner,
     cargo: [],
   };
-
-  return datastore.save({ key, data: newBoat }).then(() => key);
+  await datastore.save({ key, data: newBoat });
+  return key;
 }
 
 async function getBoatCountOfUserID(userID) {
   const query = datastore.createQuery(BOATS).filter('owner', '=', userID);
-
-  return datastore.runQuery(query).then((entities) => {
-    const count = Object.keys(entities[0]).length;
-
-    return count;
-  });
+  const entities = await datastore.runQuery(query);
+  const count = Object.keys(entities[0]).length;
+  return count;
 }
 
 // Gets all boats in the datastore
 async function getBoats(req) {
   let query = datastore.createQuery(BOATS).limit(5);
-
   if (Object.keys(req.query).includes('cursor')) {
     query = query.start(req.query.cursor);
   }
-
-  return datastore.runQuery(query).then((entities) => {
-    const results = {};
-    results.boats = entities[0].map(setBoatData);
-
-    if (entities[1].moreResults !== datastore.NO_MORE_RESULTS) {
-      const next = `${req.protocol}://${req.get('host') + req.baseUrl}?cursor=${
-        entities[1].endCursor
-      }`;
-      const editedNext = new URL(next.slice(0, -1));
-
-      results.next = `${editedNext}`;
-    }
-    return results;
-  });
-}
-
-// Gets all boats in the datastore
-async function getPublicBoats(req) {
-  let query = datastore.createQuery(BOATS).filter('public', '=', true).limit(5);
-
-  if (Object.keys(req.query).includes('cursor')) {
-    query = query.start(req.query.cursor);
+  const entities = await datastore.runQuery(query);
+  const results = {};
+  results.boats = entities[0].map(setBoatData);
+  if (entities[1].moreResults !== datastore.NO_MORE_RESULTS) {
+    const next = `${req.protocol}://${req.get('host') + req.baseUrl}?cursor=${entities[1].endCursor}`;
+    const editedNext = new URL(next.slice(0, -1));
+    results.next = `${editedNext}`;
   }
-
-  return datastore.runQuery(query).then((entities) => {
-    const results = {};
-    results.boats = entities[0].map(setBoatData);
-
-    if (entities[1].moreResults !== datastore.NO_MORE_RESULTS) {
-      const next = `${req.protocol}://${req.get('host') + req.baseUrl}?cursor=${
-        entities[1].endCursor
-      }`;
-      const editedNext = new URL(next.slice(0, -1));
-
-      results.next = `${editedNext}`;
-    }
-    return results;
-  });
+  return results;
 }
 
 async function getAllBoatsOfOwnerID(req, userID) {
-  let query = datastore.createQuery(BOATS).filter('owner', '=', userID).limit(5);
-
+  let query = datastore
+    .createQuery(BOATS)
+    .filter('owner', '=', userID)
+    .limit(5);
   if (Object.keys(req.query).includes('cursor')) {
     query = query.start(req.query.cursor);
   }
-
-  return datastore.runQuery(query).then((entities) => {
-    const results = {};
-    results.boats = entities[0].map(setBoatData);
-
-    if (entities[1].moreResults !== datastore.NO_MORE_RESULTS) {
-      const next = `${req.protocol}://${req.get('host') + req.baseUrl}?cursor=${
-        entities[1].endCursor
-      }`;
-      const editedNext = new URL(next.slice(0, -1));
-
-      results.next = `${editedNext}`;
-    }
-    return results;
-  });
+  const entities = await datastore.runQuery(query);
+  const results = {};
+  results.boats = entities[0].map(setBoatData);
+  if (entities[1].moreResults !== datastore.NO_MORE_RESULTS) {
+    const next = `${req.protocol}://${req.get('host') + req.baseUrl}?cursor=${entities[1].endCursor}`;
+    const editedNext = new URL(next.slice(0, -1));
+    results.next = `${editedNext}`;
+  }
+  return results;
 }
 
 // Returns a specific boat with a boat id of 'id'
 async function getSpecificBoat(id) {
   const key = datastore.key([BOATS, parseInt(id, 10)]);
   const query = datastore.createQuery(BOATS).filter('__key__', '=', key);
-
-  return datastore.runQuery(query).then((results) => {
+  const results = await datastore.runQuery(query)
     const selectedBoat = results[0].map(setBoatData);
     return selectedBoat;
-  });
 }
 
 async function getSpecificBoatCargo(boatCargo) {
   const allCargo = [];
-
-  for await (const cargo of boatCargo) {
-    getSpecificCargo(cargo.id).then((specificCargo) => {
+  for  (const cargo of boatCargo) {
+    const specificCargo = await getSpecificCargo(cargo.id)
       allCargo.push(specificCargo[0]);
-    });
   }
   return allCargo;
 }
@@ -137,7 +91,7 @@ async function getSpecificBoatCargo(boatCargo) {
 async function patchBoat(bid, name, type, length) {
   const key = datastore.key([BOATS, parseInt(bid, 10)]);
 
-  return getSpecificBoat(bid).then((boat) => {
+  const boat = await getSpecificBoat(bid)
     const updatedBoat = boat[0];
     if (name != null) {
       updatedBoat.name = name;
@@ -148,13 +102,14 @@ async function patchBoat(bid, name, type, length) {
     if (length != null) {
       updatedBoat.length = length;
     }
-    return datastore.save({ key, data: updatedBoat }).then(() => getSpecificBoat(bid));
-  });
+    await datastore.save({ key, data: updatedBoat });
+    return await getSpecificBoat(bid);
+
 }
 
 // Deletes selected boat with id of 'id' in the datastore
-function deleteBoats(id) {
-  const key = datastore.key([BOATS, parseInt(id, 10)]);
+async function deleteBoats(id) {
+  const key = await datastore.key([BOATS, parseInt(id, 10)]);
   return datastore.delete(key);
 }
 
@@ -162,7 +117,6 @@ module.exports = {
   setBoatData,
   getBoats,
   getBoatCountOfUserID,
-  getPublicBoats,
   getAllBoatsOfOwnerID,
   getSpecificBoat,
   getSpecificBoatCargo,
